b1e76288d5ff4f4d215d744034105377445a4f81
nikolai <nikolai@ninja25.d22.lan>
2015-10-05 07:11:28 -0500

    Datatypes

---
+++ src/jones_4_datatypes/Jones_4_datatypes.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jones_4_datatypes;
+
+/**
+ *
+ * @author nikolai
+ */
+public class Jones_4_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This is a space for comments
+        // Used to explain code
+        // Code explanation is an expectation
+        // In D22
+        int i = 512;
+        float a = 7.11f;
+        double b = 11.0;
+        short c = 100;
+        char d1 = '1';
+        char d2 = 'd';
+        char d3 = 122;
+        String e = "why certaintly";
+        String e1 = "Nikolai for President";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        /*
+        Looking at mathematical operators
+        */
+        // * This adds two numbers and more.
+        // - This subtracts two numbers and more.
+        // * This multiplies two numbers and more.
+        // / This divides two numbers and more.
+        // % This is modulo. Shows you the remainder.
+        
+        // System.out.println(i + " an integer");
+        // System.out.println(a + " a float");
+        // System.out.println(b + " a double");
+        // System.out.println(c + " a short");
+        // System.out.println(d1 + " a char");
+        // System.out.println(d2 + " a char");
+        // System.out.println(d3 + " a char");
+        // System.out.println(e + " a string");
+        // System.out.println(f + " a boolean");
+        //for(i = -123;i < 123; i++) {
+        //  System.out.println(i + "Things");
+        //   System.out.println(i % 2 == 0);
+       
+        // % checks for the remainder
+        // %2 checks if a number is 
+        // even or odd */
+    
+        // a number value for char
+        // maps to a character
+        // i = 22;
+        // a = 0;
+        // e = "ninja";
+          // System.out.println(e + i + " an /"
+          // + "a string plus an integer"
+          // ); 
+       /* adding an int and a string
+       // concatenates (combines) the int
+       // and string as astring
+       */
+       // a = 2.5f;
+       // b = 3.2;
+       // System.out.println( a + " a float/ "
+       //      + "plus a double" );
+       
+       // b = 3.1;
+       // c = 16;
+       
+       // System.out.println(b + c + " a double/"
+       // + " plus a short "
+       // );
+        
+      //* for ( i = 0; i < 100; i++) {
+           /* if is a conditional "flow"
+           statement. If a condition is true
+           the following command will be
+           executed
+           */
+          //* if (i % 2 == 0){
+        //       System.out.println(i + " not prime");
+          // } else {
+            //   System.out.println(i + " is prime");
+//           }
+  //     }
+     //    }
+   // }   
